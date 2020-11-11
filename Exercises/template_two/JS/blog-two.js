5c1980f76b41c2347c6cbd8b6195d24861c84cff
behnazsheikhi <behnaz.sheikhi@gmail.com>
2020-11-11 17:43:59 +0330

    design second-template

--- Exercises/template_two/JS/blog-two.js
+++ Exercises/template_two/JS/blog-two.js
@@ -1,8 +1,4 @@
-var modal = document.getElementById("myModal");
-
-// var span = document.getElementsByClassName("close")[0];
-
-
+// modal
 var modal = document.getElementById("blog-modal");
 
 function openModal() {
@@ -16,4 +12,44 @@ window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
+}
+
+// sidebar
+var sidebarVar = document.getElementById("sidebar");
+
+function openSidebar() {
+
+    sidebarVar.style.display = "block"
+}
+
+function closeSidebar() {
+    sidebarVar.style.display = "none";
+}
+
+function validateForm() {
+    var inputValue = document.getElementsByClassName("contact-input");
+    console.log(inputValue);
+    for (var i = 0; i <= inputValue.length; i++) {
+        if (inputValue[i].value() == "") {
+
+        }
+    }
+}
+var GoTop = document.getElementById('arrow-go-to');
+var arrowUp = document.getElementById('arrow-up');
+arrowUp.onmouseover = function showGoTop() {
+    GoTop.style.display = "block";
+}
+
+arrowUp.onmouseleave = function showGoTop() {
+    GoTop.style.display = "none";
+}
+
+
+GoTop.onmouseover = function showGoTop() {
+    GoTop.style.display = "block";
+}
+
+GoTop.onmouseleave = function showGoTop() {
+    GoTop.style.display = "none";
 }
