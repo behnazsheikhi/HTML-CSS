1a223fcaf15c6e4601a8b4b4b9cd0b25379a692b
behnazsheikhi <behnaz.sheikhi@gmail.com>
2020-11-21 09:19:48 +0330

    design mostaghel header

---
+++ Exercises/mostaghel/JS/mostaghel.js
@@ -0,0 +1,14 @@
+var menu = document.getElementById('menu-responsive');
+
+function openMenu() {
+    if (menu.style.display == "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        menu.style.display = "none";
+    }
+}
