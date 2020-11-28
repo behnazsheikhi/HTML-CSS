a543521c5d8c0086e33c383ea259a8dfa42307d0
behnazsheikhi <behnaz.sheikhi@gmail.com>
2020-11-28 10:05:33 +0330

    mostaghel

--- Exercises/mostaghel/JS/mostaghel.js
+++ Exercises/mostaghel/JS/mostaghel.js
@@ -7,3 +7,25 @@ function openMenu() {
         menu.style.display = "block";
     }
 }
+
+
+var rightBtn = document.getElementById('right-list');
+var leftBtn = document.getElementById('left-list');
+
+var right = document.getElementById('r-item-list');
+var left = document.getElementById('l-item-list');
+
+function openRightList() {
+    leftBtn.style.backgroundColor = "#ffffff";
+    left.style.display = "none";
+    rightBtn.style.backgroundColor = "#efefef";
+    right.style.display = "block";
+}
+
+function openLeftList() {
+
+    rightBtn.style.backgroundColor = "#ffffff";
+    right.style.display = "none";
+    leftBtn.style.backgroundColor = "#efefef";
+    left.style.display = "block";
+}
