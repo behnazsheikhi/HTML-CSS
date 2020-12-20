bc0c4719526de9a508b1f24f1aaabff4600d4c1a
behnazsheikhi <behnaz.sheikhi@gmail.com>
2020-12-20 09:42:39 +0330

    lightslider

---
+++ Exercises/mostaghel/JS/modtaghel-jQuery.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+
+    // var slider = $('.lightslider-gallery').lightSlider({
+    //     controls: false,
+    // });
+
+
+
+    // $(".lslide").css({ marginLeft: "10px", marginRight: "0px" });
+    // $(".lSAction").hide();
+    // $(".lSPrev").css("display", "none");
+    // $(".lSNext").css("display", "none");
+    $("#lightslider").lightSlider({
+
+
+        // loop: true,
+        // speed: 100,
+        item: 1,
+        // auto: true,
+        keypress: true,
+        // loop: true,
+        // onSliderLoad: function() {
+        //     $('#lightslider').removeClass("cS-hidden");
+        // },
+
+    });
+
+});
--- Exercises/mostaghel/JS/mostaghel.js
+++ Exercises/mostaghel/JS/mostaghel.js
@@ -29,3 +29,30 @@ function openLeftList() {
     leftBtn.style.backgroundColor = "#efefef";
     left.style.display = "block";
 }
+
+var slideIndex = 3;
+showSlides(slideIndex);
+
+// function plusSlides(n) {
+//     showSlides(slideIndex += n);
+// }
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("slider-elements");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
