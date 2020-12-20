5e455bed20dbb3e9851759342bf937d237962fe6
behnazsheikhi <behnaz.sheikhi@gmail.com>
2020-12-20 14:29:36 +0330

    toggle

--- Exercises/mostaghel/JS/modtaghel-jQuery.js
+++ Exercises/mostaghel/JS/modtaghel-jQuery.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
 
+    $("#open-menu-responsive").click(function() {
 
+        $("#menu-responsive").toggle();
+
+    });
 
     $("#lightslider").lightSlider({
         item: 1,
         keypress: true,
     });
 
-
-
     $(".second-bottom-list").slimScroll({
         height: '700px',
         size: '5px',
         position: 'right',
         color: '#952328',
-        // alwaysVisible: true,
         railVisible: true,
         railColor: '#222',
         railOpacity: 0.3,
-        // wheelStep: 10,
         allowPageScroll: true,
         disableFadeOut: true
     });
